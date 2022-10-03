4b504cd41cb1c5b710ea835cfd18d3c79d68385d
Thomas-Herner1 <tuj49289@temple.edu>
2022-10-02 20:05:54 -0400

    Closes arduino connection and catches board not found error.

--- gsrConnectionDemo.py
+++ gsrConnectionDemo.py
@@ -3,7 +3,14 @@ import time
 
 
 def main_func():
-    arduino = serial.Serial('com3', 9600)
-    arduinoRead = arduino.readline()
-    decodedRead = str(arduinoRead[0:len(arduinoRead)].decode("utf-8"))
-    list_values = decodedRead.split('sensorValue=')
+    try:
+        arduino = serial.Serial('com3', 9600)
+        arduinoRead = arduino.readline()
+        decodedRead = str(arduinoRead[0:len(arduinoRead)].decode("utf-8"))
+        list_values = decodedRead.split('sensorValue=')
+        arduinoClose = arduino.close()
+    except (OSError):
+        print("No Board Found")
+
+if __name__ == '__main__':
+    main_func()
