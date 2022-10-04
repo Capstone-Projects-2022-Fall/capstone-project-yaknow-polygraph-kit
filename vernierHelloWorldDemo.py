1b816ed05987eb8c50e02e23b75315f3481f7cb9
Thomas-Herner1 <tuj49289@temple.edu>
2022-10-04 18:33:20 -0400

    Added Logging

--- vernierHelloWorldDemo.py
+++ vernierHelloWorldDemo.py
@@ -1,9 +1,22 @@
 from gdx import gdx
 import tensorflow as tensorflow
+import logging
+
+with open('polygraphExamKitLogging.log', 'w'):
+    pass
+
+log_format = '%(asctime)s %(filename)s - %(levelname)s: %(message)s'
+
+logging.basicConfig(filename='polygraphExamKitLogging.log', level=logging.DEBUG, force=True, format=log_format, datefmt='%H:%M:%S')
 
 theAPIs = gdx()
 
-theAPIs.open_usb()
+devicesFound = theAPIs.open_usb()
+if devicesFound is None:
+    logging.error('No Device connected.')
+else:
+    logging.info('Devices found:' + devicesFound)
+
 theAPIs.select_sensors()
 theAPIs.start()
 deviceMeasurements = theAPIs.read()
@@ -11,4 +24,3 @@ theAPIs.stop()
 theAPIs.close()
 
 print("Tensorflow running as version", tensorflow.__version__)
-
