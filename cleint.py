b16b79fcf404b0194a3c967f715c27b7308ae915
tuj45886 <58963931+tuj45886@users.noreply.github.com>
2022-10-10 02:58:47 -0400

    arbituary server client communication works

---
+++ cleint.py
@@ -0,0 +1,34 @@
+import socket
+
+# IP = socket.gethostbyname(socket.gethostname())
+# IP = "127.0.1.1"
+IP = "192.168.1.159"
+PORT = 4455
+ADDR = (IP, PORT)
+FORMAT = "utf-8"
+SIZE = 1024
+
+
+def main():
+    """ Staring a TCP socket. """
+    client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+    """ Connecting to the server. """
+    client.connect(ADDR)
+
+    """ Opening and reading the file data. """
+    data = ""
+
+    while data != "-1":
+        data = input(":")
+        """ Sending the file data to the server. """
+        client.send(data.encode(FORMAT))
+        msg = client.recv(SIZE).decode(FORMAT)
+        print(f"[SERVER]: {msg}")
+
+    """ Closing the connection from the server. """
+    client.close()
+
+
+if __name__ == "__main__":
+    main()
---
+++ server.py
@@ -0,0 +1,43 @@
+import socket
+
+IP = socket.gethostbyname(socket.gethostname())
+PORT = 4455
+ADDR = (IP, PORT)
+SIZE = 1024
+FORMAT = "utf-8"
+
+
+def main():
+
+    print("Port: " + str(PORT) + "\nIP: " + str(IP) + "\nAddress: " + str(ADDR))
+    print("[STARTING] Server is starting.")
+    """ Staring a TCP socket. """
+    server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+    """ Bind the IP and PORT to the server. """
+    server.bind(ADDR)
+
+    """ Server is listening, i.e., server is now waiting for the client to connected. """
+    server.listen()
+    print("[LISTENING] Server is listening.")
+
+    msg = ""
+    """ Server has accepted the connection from the client. """
+    conn, addr = server.accept()
+    # print(f"[NEW CONNECTION] {addr} connected.")
+    print("New Connection" + str(addr))
+
+    while msg != "-1":
+        """ Receiving the filename from the client. """
+        msg = conn.recv(SIZE).decode(FORMAT)
+        # print(f"[RECV] message: {msg}")
+        print("[RECV] message: " + msg)
+        conn.send("Message received.".encode(FORMAT))
+
+    """ Closing the connection from the client. """
+    conn.close()
+    print("[DISCONNECTED] disconnected form: " + str(addr))
+
+
+if __name__ == "__main__":
+    main()
