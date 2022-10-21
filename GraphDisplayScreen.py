c036ca7b7b06a052a3f8caf214aa7654f9fb4823
Matthew Chung <tuk87051@temple.edu>
2022-10-20 22:31:17 -0400

    Added another graph to see how many the window can fit. Included dummy data to test graphing of individual graphs. Started animating real time graphing in which the graph will update every time new data is inputted into the csv file

--- GraphDisplayScreen.py
+++ GraphDisplayScreen.py
@@ -1,4 +1,8 @@
 from matplotlib import pyplot as plt
+import csv
+import matplotlib.animation as animation
+import datetime
+import time
 
 BPM = [65,67,70,72,80,85,90,95,110,115]
 TIME = [1,2,3,4,5,6,7,8,9,10]
@@ -7,20 +11,51 @@ SIEMENS = [2, 3, 4, 4.6, 6, 5.3, 4.4, 4, 5, 6.8]
 
 RESPIRATION = [12, 12, 13, 15, 14, 17, 16, 15, 15, 20]
 
-fig, (graph0, graph1, graph2) = plt.subplots(nrows= 3, ncols=1)
+fig, graph= plt.subplots(2, 2, sharex=True)
 
-graph0.plot(TIME, BPM)
-graph0.set_xlabel("Time (minutes)")
-graph0.set_ylabel("BPM")
+fig.subplots_adjust(hspace=0)
 
-graph1.plot(TIME, SIEMENS)
-graph1.set_xlabel("Time (minutes)")
-graph1.set_ylabel("Siemens")
+#CSVPath = str(pathlib.Path().absolute())+'\TestData.csv'
 
-graph2.plot(TIME, RESPIRATION)
-graph2.set_xlabel("Time (minutes)")
-graph2.set_ylabel("Respiration")
+'''f = open("test.csv", 'w')
+CurrentTime = datetime.datetime.now()
+CurrentTimeOrg = CurrentTime.strftime("%H:%M:%S")
+counter = 0
+while counter < 10:
+    f.write(str(CurrentTimeOrg)+"\n")
+    time.sleep(1)
+    counter+=1
+    print(counter)'''
 
-plt.tight_layout()
+
+def animate(i):
+    x = []
+    y = []
+    with open('TestData.csv', 'r') as csvfile:
+        lines = csv.reader(csvfile, delimiter=',')
+        for row in lines:
+            x.append(row[0])
+            y.append(int(row[1]))
+
+        graph[0,0].clear()
+        graph[1,0].clear()
+        graph[0,1].clear()
+        graph[1,1].clear()
+
+        graph[0,0].plot(x, y, color='b', marker='o')
+        graph[0,0].set_ylabel("BPM")
+
+        graph[1,0].plot(x, y, color='k', marker='o')
+        graph[1,0].set_ylabel("Siemens")
+
+        graph[0,1].plot(x, y, color='g', marker='o')
+        graph[0,1].set_ylabel("Respiration")
+
+        graph[1,1].plot(x, y, color='b', marker='o')
+        graph[1,1].set_ylabel("BPM")
+
+ani = animation.FuncAnimation(fig, animate, interval=1000)
 
 plt.show()
+
+
