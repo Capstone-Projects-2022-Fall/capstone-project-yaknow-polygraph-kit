3585cb246473739834dab2eaac0d975bb7d7d8fb
tuj45886 <58963931+tuj45886@users.noreply.github.com>
2022-10-28 17:03:06 -0400

    connection to cis-wang2 in progress

---
+++ client.py
@@ -0,0 +1,43 @@
+import socket
+
+#hostname -I
+IP = "129.32.22.10"
+PORT = 30001
+ADDR = (IP, PORT)
+FORMAT = "utf-8"
+SIZE = 1024
+
+
+def main():
+    """ Staring a TCP socket. """
+    client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    # permission net permitted for some reason
+    # socket.sethostname("cis-linux2.temple.edu")
+
+    """ Connecting to the server. """
+    client.connect(ADDR)
+
+    """ Opening and reading the file data. """
+    data = ""
+
+    while data != "-1":
+        data = input(":")
+        """ Sending the file data to the server. """
+        client.send(data.encode(FORMAT))
+        """ 
+        //ssh into cis linux 2 directly instead of 
+        //we should be good to go since we have 
+        
+        
+        
+        
+        """
+        msg = client.recv(SIZE).decode(FORMAT)
+        print("[SERVER]: " + msg)
+
+    """ Closing the connection from the server. """
+    client.close()
+
+
+if __name__ == "__main__":
+    main()
---
+++ server.py
@@ -0,0 +1,48 @@
+import socket
+import os
+
+IP = "129.32.22.10"
+PORT = 30001
+ADDR = (IP, PORT)
+SIZE = 1024
+FORMAT = "utf-8"
+
+
+def main():
+    print("Port: " + str(PORT) + "\nIP: " + str(IP) + "\nAddress: " + str(ADDR))
+    print("[STARTING] Server is starting.")
+    """ Staring a TCP socket. """
+    server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+    """ Bind the IP and PORT to the server. """
+    server.bind(ADDR)
+
+    """ Server is listening, i.e., server is now waiting for the client to connected. """
+    server.listen()
+    print("[LISTENING] Server is listening.")
+
+    msg = ""
+    """ Server has accepted the connection from the client. """
+    conn, addr = server.accept()
+    # print(f"[NEW CONNECTION] {addr} connected.")
+    print("New Connection" + str(addr))
+
+    while msg != "-1":
+        """ Receiving the filename from the client. """
+        msg = conn.recv(SIZE).decode(FORMAT)
+        # print(f"[RECV] message: {msg}")
+        if "$" in msg:
+            msg = msg.replace('$', '')
+            print("Command: " + msg)
+            os.system(msg)
+        else:
+            print("[RECV] message: " + msg)
+        conn.send("Message received.".encode(FORMAT))
+
+    """ Closing the connection from the client. """
+    conn.close()
+    print("[DISCONNECTED] disconnected form: " + str(addr))
+
+
+if __name__ == "__main__":
+    main()
