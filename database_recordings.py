fe401b574cdca1e452f26d814aedc60e54c4ed79
Keshav Saraogi <78319655+KeshavSaraogi@users.noreply.github.com>
2022-10-29 12:14:08 -0400

    Update database_recordings.py
    -- suggesting new db

--- database_recordings.py
+++ database_recordings.py
@@ -7,14 +7,15 @@ db = mysql.connector.connect(
     database="Recordings"
 )
 
-#CREATE TABLE `SingularRecording` (
-#	`question` TEXT,
-#	`response` CHAR,
-#	`timestamp` DATETIME,
-#	`blood_pressure` DECIMAL,
-#	`skin_conductivity` DECIMAL,
-#	`respiration_belt` DECIMAL,
-#	`pulse` DECIMAL,
-#	`id` INT NOT NULL AUTO_INCREMENT
-#);
+mycursor = db.execute()
+mycursor.execute("CREATE TABLE `SingularRecording` (`question` TEXT,`response` CHAR,`timestamp` DATETIME,`pulse` DECIMAL,`skin_conductivity` DECIMAL,`respiration_belt` DECIMAL,`blood_pressure` DECIMAL")
 
+class SingluarRecording:
+    def __init__(self, timestamp, question, response, skin_conductivity, respiration_belt, blood_pressure, pulse):
+        self.timestamp = timestamp
+        self.question = question
+        self.response = response
+        self.skin_conductivity = skin_conductivity
+        self.respiration_belt = respiration_belt
+        self.blood_pressure = blood_pressure
+        self.pulse = pulse
