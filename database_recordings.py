7fae36e9c178c95678f3a1fab6a91430e7a875cd
tuj45886 <58963931+tuj45886@users.noreply.github.com>
2022-10-29 13:54:31 -0400

    created the db using work bench

--- database_recordings.py
+++ database_recordings.py
@@ -1,14 +1,32 @@
 import mysql
+import mysql.connector
 
 db = mysql.connector.connect(
     host="173.255.232.150",
     user="cis4398",
     passwd="dNC=IK~9)7",
-    database="Recordings"
+    database="SingularRecording"
 )
 
 mycursor = db.execute()
-mycursor.execute("CREATE TABLE `SingularRecording` (`question` TEXT,`response` CHAR,`timestamp` DATETIME,`pulse` DECIMAL,`skin_conductivity` DECIMAL,`respiration_belt` DECIMAL,`blood_pressure` DECIMAL")
+mycursor.execute("CREATE TABLE `SingularRecording` (`exmaID` DECIMAL,`questionID` DECIMAL,`question` TEXT,`response` CHAR,`timestamp` DATETIME,`pulse` DECIMAL,`skin_conductivity` DECIMAL,`respiration_belt` DECIMAL,`blood_pressure` DECIMAL")
+
+""""
+CREATE TABLE `Questions`.`SingularRecording` (
+  `exmaID` INT NOT NULL,
+  `questionID` INT NOT NULL,
+  `question` VARCHAR(255) NULL,
+  `response` VARCHAR(255) NULL DEFAULT 'no response',
+  `timestamp` DATETIME NOT NULL,
+  `pulse` VARCHAR(255) NULL,
+  `skin_conductivity` INT NULL,
+  `respiration_belt` VARCHAR(45) NULL,
+  `blood_pressure` VARCHAR(45) NULL,
+  UNIQUE INDEX `timestamp_UNIQUE` (`timestamp` ASC));
+"""""
+
+
+
 
 class SingluarRecording:
     def __init__(self, timestamp, question, response, skin_conductivity, respiration_belt, blood_pressure, pulse):
