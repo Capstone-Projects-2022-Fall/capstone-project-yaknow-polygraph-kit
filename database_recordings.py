67df7257aee95c95bc86dd2d4d8eeefd782df67d
Keshav Saraogi <78319655+KeshavSaraogi@users.noreply.github.com>
2022-10-29 11:03:51 -0400

    Create database_recordings.py
    -- creating the tables and connector

---
+++ database_recordings.py
@@ -0,0 +1,20 @@
+import mysql
+
+db = mysql.connector.connect(
+    host="173.255.232.150",
+    user="cis4398",
+    passwd="dNC=IK~9)7",
+    database="Recordings"
+)
+
+#CREATE TABLE `SingularRecording` (
+#	`question` TEXT,
+#	`response` CHAR,
+#	`timestamp` DATETIME,
+#	`blood_pressure` DECIMAL,
+#	`skin_conductivity` DECIMAL,
+#	`respiration_belt` DECIMAL,
+#	`pulse` DECIMAL,
+#	`id` INT NOT NULL AUTO_INCREMENT
+#);
+
