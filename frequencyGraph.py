1c0d42c1a016ff3ab17ea0679ebf1f6b17bdd292
Matthew Chung <tuk87051@temple.edu>
2022-10-31 02:12:57 -0400

    Updated frequency frame

--- frequencyGraph.py
+++ frequencyGraph.py
@@ -8,7 +8,9 @@ import conductExamScreen
 fig, (graph0, graph1, graph2, graph3) = plt.subplots(nrows=4, ncols=1)
 plt.subplots_adjust(bottom=0.25)
 
-binwidth = 2
+binwidthRespiration = 2
+#binwidthGSR =
+#binwidthBP =
 
 def createFrequencyGraphs():
     print("Hello??")
@@ -22,14 +24,23 @@ def createFrequencyGraphs():
         print(respirationRecording.measurement[0])
         RespirationMeasurement.append(respirationRecording.measurement[0])
 
-    graph0.hist(RespirationMeasurement, bins=np.arange(min(RespirationMeasurement), max(RespirationMeasurement) + binwidth, binwidth), color='blue', edgecolor='black')
+    graph0.hist(RespirationMeasurement, bins=np.arange(min(RespirationMeasurement), max(RespirationMeasurement) + binwidthRespiration, binwidthRespiration), color='blue', edgecolor='black')
     graph0.set_ylabel("Respiration")
 
-    #graph1.hist(GSRMeasurement, bins=range(min(GSRMeasurement), max(GSRMeasurement) + binwidth, binwidth), color='green', edgecolor='black')
+    # for skinConductivityRecording in conductExamScreen.skinConductivityRecordings:
+    #     print(skinConductivityRecording.measurement[0])
+    #     GSRMeasurement.append(skinConductivityRecording.measurement[0])
+    #graph1.hist(GSRMeasurement, bins=np.arange(min(GSRMeasurement), max(GSRMeasurement) + binwidthGSR, binwidthGSR), color='green', edgecolor='black')
     #graph1.set_ylabel("Siemens")
 
+    graph3.set_xlabel("Frequency")
+
 '''
-    graph2.hist(BPMEASUREMENT, bins=range(min(BPMEASUREMENT), max(BPMEASUREMENT) + binwidth, binwidth), color='pink', edgecolor='black')
+    for bloodPressureRecording in conductExamScreen.bloodPressureRecordings:
+        print(bloodPressureRecording.measurement[0])
+        bloodPressureMeasurement.append(bloodPressureRecording.measurement[0])   
+    
+    graph2.hist(BPMEASUREMENT, bins=np.arange(min(GSRMeasurement), max(GSRMeasurement) + binwidthBP, binwidthBP), color='pink', edgecolor='black')
     graph2.set_ylabel("Blood Pressure")
 
     graph3.hist(HEARTRATEMEASUREMENT, bins=range(min(HEARTRATEMEASUREMENT), max(HEARTRATEMEASUREMENT) + binwidth, binwidth), color='orange', edgecolor='black')
@@ -54,6 +65,6 @@ def createFrequencyGraphs():
 # graph2.hist(y, bins=range(min(y), max(y) + binwidth, binwidth), color='pink', edgecolor='black')
 # graph3.hist(y, bins=range(min(y), max(y) + binwidth, binwidth), color='orange', edgecolor='black')
 
-#createFrequencyGraphs()
+createFrequencyGraphs()
 
-#plt.show()
+plt.show()
