213ddf99e556ceee81938ad636e5d75674ee0547
tuj45886 <58963931+tuj45886@users.noreply.github.com>
2022-11-05 14:51:53 -0400

    Inserting into the Singular recording table of the db now works, it has not been integrated completely with the program yet and the deletion function needs to be fixed. Ferdinand was a great help with Bug fixing and db research

--- test_DB_IMPL.py
+++ test_DB_IMPL.py
@@ -11,4 +11,6 @@ skin_con = 3
 respBel = "34"
 bp = "43"
 
+
+
 testUpdateSingularRecording.add_singularRecord(exam_ID,  question_ID, question, response, timeStamp, pulse, skin_con, respBel, bp, "T")
