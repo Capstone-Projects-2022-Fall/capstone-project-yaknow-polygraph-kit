52b8a74d1938feea3cb429f593b3aca8632a1d30
tuj45886 <58963931+tuj45886@users.noreply.github.com>
2022-11-14 05:22:57 -0500

    Prediction is outputted to file

--- ML_Classification.py
+++ ML_Classification.py
@@ -22,11 +22,11 @@ df = pd.read_sql("SELECT exmaID, questionID, question, response, tsStamp, pulse,
 db_con.close()'''
 
 df = pd.read_csv(
-    "db_Nov13_v5_addedTestArray.csv.csv", na_values=['NA', '?'])
+    "db_Nov13_v4.csv", na_values=['NA', '?'])
 
 # Convert to numpy classificatin
 x = df[['response', 'tsStamp', 'pulse', 'skin_conductivity', 'respiration_belt',
-        'blood_pressure, trial']].values
+        'blood_pressure']].values
 dummies = pd.get_dummies(df['actual_ans'])  # Classification (labelling)
 species = dummies.columns
 y = dummies.values
@@ -42,18 +42,23 @@ model.fit(x,y,verbose=2,epochs=100)
 print(species)
 
 pred = model.predict(x)
-print(f"Shape: {pred.shape}")
-print(pred[0:10])
+#print(f"Shape: {pred.shape}")
 np.set_printoptions(suppress=True)
-print(y[0:10])
 predict_classes = np.argmax(pred,axis=1)
 expected_classes = np.argmax(y,axis=1)
-print(f"Predictions: {predict_classes}")
-print(f"Expected: {expected_classes}")
+#print(f"Predictions: {predict_classes}")
+#print(f"Expected: {expected_classes}")
 
 print(species[predict_classes[1:10]])
 
 correct = accuracy_score(expected_classes,predict_classes)
 print(f"Accuracy: {correct}")
+
+sample_flower = np.array( [['0', '93.032147', '0', '0', '26.402095794677734', '159.7682222']], dtype=float)
+pred = model.predict(sample_flower)
+print(pred)#False , True
+pred = np.argmax(pred)
+print(f"Predict that {sample_flower} is: {species[pred]}")
+os.system(f"echo  {species[pred]} with a accuracy of {pred} > prediction.txt")
 
 
