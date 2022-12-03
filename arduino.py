9cdd934c9e14947c8720bb73f76659d6440c3287
Thomas-Herner1 <tuj49289@temple.edu>
2022-12-03 18:32:42 -0500

    -Additional sleep statements when device threads waiting to allow for more accurate blood pressure bar reading.

--- arduino.py
+++ arduino.py
@@ -9,9 +9,9 @@ import conductExamScreen
 
 baud = 9600
 file_name = "analog_data.csv"
-arduino_port = "/dev/cu.usbmodem14101"
+#arduino_port = "/dev/cu.usbmodem14101"
 #arduino_port = "COM1"
-#arduino_port = "COM4"
+arduino_port = "COM4"
 arduino_port2 = "/dev/cu.usbmodem101"
 
 
@@ -59,6 +59,8 @@ def connectGSRSensor():
                 print("Failed GSR")
                 time.sleep(rate)
                 print(final_reading)
+            else:
+                time.sleep(.5)
     print("GSR Exited")
 
 
