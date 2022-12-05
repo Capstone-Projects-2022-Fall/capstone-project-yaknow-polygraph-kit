d34219b039d97deb95e8045980825a9d726a4de3
Keshav Saraogi <78319655+KeshavSaraogi@users.noreply.github.com>
2022-12-04 23:06:25 -0500

    php files
    -- files for the website

---
+++ about.php
@@ -0,0 +1,86 @@
+<!DOCTYPE html>
+<html lang="en">
+    <?php include('templates/header.php') ?>
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <style>
+        <?php include ('styles/index.css')?>
+        <?php include ('styles/welcome.css') ?>
+    </style>
+    <div class="about-section">
+        <h1>Polygraph Test Kit</h1>
+        <h6>The yaKnow Polygraph Kit is an improvised and alternative polygraph device used by an examiner to measure physiological responses of an examinee while the examinee is asked a series of questions. </h6>
+        <h6>After the measurements are collected and recorded, the examiner interprets the collected data set to determine if the examinee was engaging in deception during question. </h6>
+    </div>
+
+    <h2 style="text-align:center">Meet Our Team</h2>
+    <div class="row">
+        <div class="column">
+            <div class="card">
+                <img src="images/img3.jpg" alt="Thomas" style="width:100%">
+                <div class="container">
+                    <h2>Thomas</h2>
+                    <p><a button class="button" href="mailto:tuj49289@temple.edu" >Contact</a></p>
+                </div>
+            </div>
+        </div>
+
+        <div class="column">
+            <div class="card">
+                <img src="images/img3.jpg" alt="Ferdinand" style="width:100%">
+                <div class="container">
+                    <h2>Ferdinand</h2>
+                    <p><a button class="button" href="mailto:tuj45886@temple.edu">Contact</a></p>
+                </div>
+            </div>
+        </div>
+
+        <div class="column">
+            <div class="card">
+                <img src="images/img3.jpg" alt="Caleb" style="width:100%">
+                <div class="container">
+                    <h2>Caleb</h2>
+                    <p><a button class="button" href="mailto:tuj45886@temple.edu">Contact</a></p>
+                </div>
+            </div>
+        </div>
+
+        <div class="column">
+            <div class="card">
+                <img src="images/img3.jpg" alt="Mathew" style="width:100%">
+                <div class="container">
+                    <h2>Mathew</h2>
+                    <p><a button class="button" href="mailto:tuk87051@temple.edu">Contact</a></p>
+                </div>
+            </div>
+        </div>
+
+        <div class="column">
+            <div class="card">
+                <img src="images/img3.jpg" alt="Alec" style="width:100%">
+                <div class="container">
+                    <h2>Alec</h2>
+                    <p><a button class="button" href="mailto:mike@example.com">Contact</a></p>
+                </div>
+            </div>
+        </div>
+
+        <div class="column">
+            <div class="card">
+                <img src="images/img3.jpg" alt="Keshav" style="width:100%">
+                <div class="container">
+                    <h2>Keshav</h2>
+                    <p><a button class="button" href="mailto:tuj49866@temple.edu">Contact</a></p>
+                </div>
+            </div>
+        </div>
+    
+    </div>
+    <?php include('templates/footer.php') ?>
+</body>
+</html>
